fe18fda3832b416fc7d8489314f78caa43ebb714
Legaljay <adesinaayobami67@gmail.com>
2023-12-11 10:30:16 +0100

    all docs

---
+++ app/auth/login/page.jsx
@@ -0,0 +1,12 @@
+import styles from '@/app/ui/login/login.module.css';
+import LoginForm from "../../ui/login/loginForm/loginForm";
+
+const LoginPage = () => {
+    return (
+        <div>
+            <LoginForm />
+        </div>
+    )
+}
+
+export default LoginPage;
